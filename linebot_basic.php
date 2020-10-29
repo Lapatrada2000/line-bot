0e80b0f485a07a2c4a5ad649fa39b0b2b51dfa76
Lapatrada2000 <71629112+Lapatrada2000@users.noreply.github.com>
2020-10-29 16:57:49 +0700

    Update linebot_basic.php

--- linebot_basic.php
+++ linebot_basic.php
@@ -16,7 +16,7 @@ foreach ($request_json['events'] as $event)
 			
 			$reply_message = 'ฉันได้รับข้อความ '. $text.' ของคุณแล้ว!'; 
 			
-			$reply_message = mySQL_selectAll('http://bot.kantit.com/json_select_users.php');
+			$reply_message = mySQL_selectAll('http://s61160071.kantit.com/json_select.php');
 			
 			
 		} else {
